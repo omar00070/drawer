7ba31d1703ad017953a63a292d203c9caa78ed61
omar <omar_omarsalama@yahoo.com>
2020-07-14 13:59:19 +0200

    adding drawing ability

--- app.js
+++ app.js
@@ -2,17 +2,50 @@
 const canvas = document.querySelector('canvas');
 const topBar = document.querySelector('.top-bar');
 const dropBox = document.querySelector('.colors-container');
+const colorBox = document.querySelector('.color-square');
+const pencilSize = document.querySelector('select');
+const colorPlace = document.querySelector('.color-place')
+
 
 //global vars
 const topBarHeight = topBar.offsetHeight;
 let colorsNumber = 45;
+let color = 'rgb(0, 0, 0)';
+let is_drawing = false;
+let mouse_position = {
+    x:  undefined,
+    y:  undefined
+};
+let size = 5;
+
+const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight - topBarHeight;
 
-console.log(topBarHeight)
-
 //event listeners
+dropBox.addEventListener('click', chooseColor);
 
+document.addEventListener('mousedown', function(e){
+    if(e.target.tagName == 'CANVAS'){
+        is_drawing = true
+    }
+});
+
+document.addEventListener('mouseup', function(){
+    is_drawing = false;
+});
+
+document.addEventListener('mousemove', function(e){
+    mouse_position = {
+        x:  e.x,
+        y:  e.y
+    }
+});
+
+pencilSize.addEventListener('click', function(e){
+    size = e.target.value
+    console.log(size)
+});
 
 //functions
 function dropDown(){
@@ -55,11 +88,29 @@ function fillColors(){
         colorBtn.classList.add('color-square');
         colorDiv.appendChild(colorBtn);
         colorBtn.style.background = backgroundColor
-        console.log(backgroundColor)
         dropBox.appendChild(colorDiv)        
     }
 
-    console.log(colors);
-}
+};
 
+function chooseColor(e){
+    color = e.target.style.background
+    dropBox.style.display = 'none'
+    colorPlace.style.background = color
+};
+
+function drawing(){
+    c.beginPath();
+    c.arc(mouse_position.x, mouse_position.y - topBar.offsetHeight, size, 0, Math.PI * 2, false);
+    c.fillStyle = color;
+    c.fill();
+};
+
+function animate(){
+    requestAnimationFrame(animate);
+    if(is_drawing){
+        drawing();
+    };
+};
+animate();
 fillColors();
